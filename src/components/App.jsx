1b716681030c0f503fa5244f4c95be32f2b6f0cc
Siarhei Karpovich1 <skarpovich@cloudpassage.com>
2022-08-16 23:09:59 +0300

    Added Redux

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,22 @@
-import React from "react";
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { setCount } from '../reducers/reposReducer';
+import './app.less';
+
 
 const App = () => {
+  const dispatch = useDispatch();
+  const count = useSelector(state => state.repos.count)
+
+  function onCountClick() {
+    dispatch(setCount(5));
+  }
+
   return (
-    <div>Hello React</div>
+    <div className="app">
+      <button onClick={() => onCountClick()}>COUNT</button>
+      <div>{count}</div>
+    </div>
   );
 };
 
