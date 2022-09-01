5ef88d8bbc680be5ab6c5610201af8d2edf6b744
Siarhei Karpovich1 <skarpovich@cloudpassage.com>
2022-09-02 00:10:18 +0300

    Add search

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,20 +1,41 @@
 import React from 'react';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getRepos } from '../actions/repos';
 import Repo from './repo/Repo';
+import './main.less';
 
 const Main = () => {
   const dispatch = useDispatch();
   const repos = useSelector(state => state.repos.items);
+  const isFetching = useSelector(state => state.repos.isFetching);
+  const [searchValue, setSearchValue] = useState('');
 
   useEffect(() => {
     dispatch(getRepos());
   }, [])
 
+  const searchHandler = () => {
+    dispatch(getRepos(searchValue));
+  }
+
   return (
     <div className="app">
-      {repos.map(repo => <Repo key={repo.id} repo={repo}/>)}
+      <div className='search'>
+        <input 
+          type="text"
+          placeholder='Type repo name'
+          className='search-input'
+          value={searchValue}
+          onChange={(e) => setSearchValue(e.target.value)}
+        />
+        <button onClick={() => searchHandler()} className='search-btn'>Search</button>
+      </div>
+      { isFetching 
+        ? (
+          <div className='fetching'></div>) 
+        :
+          repos.map(repo => <Repo key={repo.id} repo={repo}/>)}
     </div>
   );
 };
